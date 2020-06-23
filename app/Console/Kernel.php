922985f4364105d3f831adabfb3c11bd385537db
Tshewang Tenzin <cheaowang143@gmail.com>
2020-06-23 23:23:20 +0600

    added backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->command('backup:clean')->daily();
+        $schedule->command('backup:run')->daily();
     }
 
     /**
