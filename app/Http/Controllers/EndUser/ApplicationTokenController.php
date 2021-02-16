0884e4687bbf47f553326ddd77b7f94a0c9f5536
Dorji Wangdi <dorjiwangdi90@gmail.com>
2021-02-16 16:26:37 +0600

    sso update

--- app/Http/Controllers/EndUser/ApplicationTokenController.php
+++ app/Http/Controllers/EndUser/ApplicationTokenController.php
@@ -11,21 +11,20 @@ class ApplicationTokenController extends Controller
     public function oAuthRedirect(Request $request){
         $queries= http_build_query([
             'client_id'=>'MRNEQWUn8AU7ii_99B_I5ihUG80a',
-            'redirect_uri'=>'https://stg-­sso.dit.gov.bt/sso/enduser_dashboard',
+            'redirect_uri'=>'https://portal.tourism.gov.bt/sso/enduser_dashboard',
             'response_type'=>'code'
         ]);
         return redirect('https://stg-­sso.dit.gov.bt/oauth2/authorize?' .$queries );
     }
-    
+
     public function callBack(Request $request){
-        dd($request->code);
         $http = new GuzzleHttp\Client;
         $response = $http->post('https://stg-­sso.dit.gov.bt/oauth2/token', [
             'form_params' => [
                 'grant_type' => 'client_credentials',
                 'client_id' => 'MRNEQWUn8AU7ii_99B_I5ihUG80a',
                 'client_secret' => 'WYUyM5ZMLYOe03CaSd4VEuUQ2f0a',
-                'redirect_uri' => 'https://stg-­sso.dit.gov.bt/sso/enduser_dashboard',
+                'redirect_uri' => 'https://portal.tourism.gov.bt/sso/enduser_dashboard',
                 'code' => $request->code,
             ],
         ]);
